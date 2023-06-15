bbc19b8b60fc51e038a74f65e439360a84eae69f
Sara-Poyo-Pastor <txara88@hotmail.com>
2023-06-16 01:41:03 +0200

    iphone

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,13 +1,17 @@
+/* eslint-disable react/prop-types */
+import React from 'react'
 import './button.css';
+import play from "../../assets/imagenes_comprimidas/Google_Play.png";
 
-function Button(){
+function Button({clasesBoton, icon, botonContenido, link}){
+    let clonedIcon = React.cloneElement(icon, {src:play, width:15, height:15})
+
     return(
-        <>
-        <button className="download_apple" type="button">
-            <a href="https://factoriaf5.org/">DOWNLOAD</a> 
-        </button>
-        </>
+<a href="https://factoriaf5.org"><button className={"Button " + clasesBoton}>
+{botonContenido} {clonedIcon} {link}
+</button>
+</a>
     )
 }
 
-export default Button
+export default Button;
--- src/pages/Landingpage.jsx
+++ src/pages/Landingpage.jsx
@@ -2,6 +2,9 @@ import '../index.css';
 import Navbar from "../components/atoms/navbar";
 import Button from "../components/button/Button";
 import Title from "../components/title/Title";
+import iconoApple from "../assets/imagenes_comprimidas/icono_apple.svg";
+import phone from "../assets/imagenes_comprimidas/header-iphone.png";
+import play from "../assets/imagenes_comprimidas/Google_Play.png";
 
 function Landingpage(){
     return(
@@ -13,12 +16,17 @@ function Landingpage(){
                         <div className="text">
                             <Title/>
                         <div className="download_button">
-                            <Button/>
-                            <Button/> 
+                            <Button clasesBoton="botonBg" botonContenido="DOWNLOAD" icon={<img src={iconoApple}/>}></Button>
+                            <Button clasesBoton="botonBg" botonContenido="DOWNLOAD" icon={<img src={play}/>}></Button> 
                         </div> 
                         </div>
                     </div>
                 </div>
+        </div>
+        <div className='iphone'>
+        <div className="container-iphone">
+            <img className="iphone_landing" src={phone}/>
+        </div>
         </div>
         </>
     )
